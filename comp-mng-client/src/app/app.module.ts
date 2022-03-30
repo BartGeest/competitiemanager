292ca32a373cb0b72d8a4b38d58a4e87977175b1
BartGeest <b.vandergeest@protonmail.com>
2022-03-30 11:51:51 +0200

    installed and tested clarity

--- comp-mng-client/src/app/app.module.ts
+++ comp-mng-client/src/app/app.module.ts
@@ -3,13 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { LoginComponent } from './components/login/login.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {ClarityModule} from "@clr/angular";
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomepageComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    ClarityModule,
     AppRoutingModule
   ],
   providers: [],
