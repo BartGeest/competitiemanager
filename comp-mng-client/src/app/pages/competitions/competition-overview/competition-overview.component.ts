93dbf04723587a0f1a1bc62af2515ffc3f2733bb
BartGeest <b.vandergeest@protonmail.com>
2022-08-25 11:11:44 +0200

    bij klikken sport tab -> tabel updaten met juiste competities next: nieuwe branch - team kunnen aanmaken als gebruiker

--- comp-mng-client/src/app/pages/competitions/competition-overview/competition-overview.component.ts
+++ comp-mng-client/src/app/pages/competitions/competition-overview/competition-overview.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
 import {PathService} from "../../../services/path/path.service";
 import {Competition} from "../../../model/Competition";
+import {CompetitionService} from "../../../services/competition/competition.service";
 
 @Component({
   selector: 'app-compeition-overview',
@@ -20,15 +21,25 @@ export class CompetitionOverviewComponent implements OnInit {
 
   competitions: Competition[] = [];
 
-  constructor(private router: Router, private route: ActivatedRoute, private path: PathService) {}
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private path: PathService,
+    private compService: CompetitionService) {}
 
   ngOnInit(): void {
     //door gebruik te maken van een resolver is er data op te halen vanuit de ActivatedRoute
     //HEEL BELANGRIJK in de subscribe moet je wel de key gebruiken die in de resolver staat bij de route in app.module.ts
+    //In dit geval is dat competitionResponse
     this.route.data.subscribe(({competitionResponse}) => {
-      console.log(competitionResponse);
       this.competitions = competitionResponse.competitions;
     })
+  }
+
+  retrieveCompetitions(sportname: string) {
+    this.compService.getCompetitionsBySport(sportname).subscribe( (competitionResponse) => {
+      this.competitions = competitionResponse.competitions;
+    });
   }
 
   navToCompParticipate(): void {
